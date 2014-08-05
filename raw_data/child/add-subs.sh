ad230a3cb47daf9da31c339a2cbe1ac5ed2b08c0
Kyle MacDonald <kyle.macdonald@stanford.edu>
2014-08-05 15:06:25 -0700

    updated data munging script

--- raw_data/child/add-subs.sh
+++ raw_data/child/add-subs.sh
@@ -1,8 +1,20 @@
-# add subject numbers to beginning of filename
+# add subject numbers to end of filename
 # files are sorted by date run
 
 count=1
 dash="-"
-expt="soc-xsit"
 
-for file in *.txt; do mv ./"$file" "$expt$dash$count$dash$file"; count=`expr $count + 1`; done
+for file in *.txt; do 
+	mv ./"$file" "$count$dash$file"; 
+	count=`expr $count + 1`; 
+done
+
+
+##### remove the subject numbers 
+
+# count=1;
+
+# for file in `ls *.txt | sort -n -t - -k 3`; do 
+# 	mv ./"$file" "${file/soc-xsit-$count-/}"; 
+# 	count=`expr $count + 1`;
+# done
