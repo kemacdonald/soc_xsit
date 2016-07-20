9d8f34456071369a89c6149f418dd22b478b3653
Kyle MacDonald <kyle.macdonald@stanford.edu>
2016-07-20 10:09:46 -0700

    cleaned up inspection time munging script

--- data/1d_e4_json2csv_data_cleaning.R
+++ data/1d_e4_json2csv_data_cleaning.R
@@ -13,6 +13,7 @@ library(jsonlite)
 library(plyr)
 library(dplyr)
 library(lubridate)
+library(magrittr)
 
 ## set path for writing data (same for all three experiments)
 write_path <- file.path("/Users", "kmacdonald", "Documents", "Projects", "SOC-XSIT", "SOC_XSIT_GIT", "data/", "2_raw_compiled/")
@@ -71,14 +72,11 @@ strip.rownames <- function(x) {
 
 trial.data <- strip.rownames(do.call(rbind, trial.data))
 
-
 ##### Merge trial level data with subject/experiment level data
 
 exp_df <- mutate(exp_df, workerid = as.character(workerid))
 trial.data <- mutate(trial.data, workerid = as.character(workerid))
-
 df_final <- left_join(exp_df, trial.data, by="workerid")
-  
 
 ####### Now start munging to create extra variables that we want for analysis reasons
 
@@ -95,74 +93,62 @@ trial_numbers <- seq(1:num_trials_exp)
 df_final <- ddply(df_final, .(workerid), function(x) {
   x$trial_num <- trial_numbers
   x
-  })
-
+  }
+)
 
 #### drop subs who have more than 36 trials
 drop.subs <- ddply(df_final,.(workerid), function(x) {nrow(x) > num_trials_exp}) %>% 
-  mutate(include = ifelse(V1 == F, "include", "exclude")) %>% 
+  mutate(include_exclude_repeat = ifelse(V1 == F, "include", "exclude")) %>% 
   select(-V1)
+
+drop.subs <- df_final %>% 
+  filter(trial_category == "example") %>% 
+  mutate(include_exclude_examples = ifelse(trial_num %in% c(1,2) & chosen == "squirrel", "include",
+                                  ifelse(trial_num %in% c(3,4) & chosen == "tomato", "include", 
+                                 "exclude"))) %>% 
+  select(workerid, include_exclude_examples) %>% 
+  distinct() %>% 
+  left_join(drop.subs, ., by = "workerid")
+  
 
 df_final <- left_join(df_final, drop.subs, by = "workerid")
 
 #### clean-up variables
-
 df_final <- df_final %>% 
   mutate(workerid = as.factor(workerid),
          trialType = factor(trialType, labels = c('Same','Switch')),
          intervalNum = ifelse(interval == "Zero", 0, 
-                              ifelse(interval == "Three", 3, NA))
-         )
+                              ifelse(interval == "Three", 3, NA)),
+         numPicN = as.numeric(numPic),
+         gaze_trial = ifelse(social_cond == "No-socialFirst" & itemNum < 8, "No-Gaze", 
+                             ifelse(social_cond == "SocialFirst" & itemNum >= 8, "No-Gaze", 
+                                    "Gaze")))
 
-#### flag trial category (example, exposure, test)
+## flag first test trials for subjects 
+df_final$first.trial <- FALSE
+df_final$first.trial[(df_final$interval=="Zero" & df_final$trial_num==5) |
+                        (df_final$interval=="Three" & df_final$trial_num==9)] <- TRUE
 
+## flag correct vs. incorrect
 
-#### exclude subjects for getting example trials wrong
-
-df_final <- df_final %>% 
-  mutate(include = ifelse(trial_num %in% c(1,2) ))
-
-
-
-example.data <- filter(all.data, trial_category == "example")
-include.subs <- ddply(example.data,.(subid),
-                      function(x) {x$chosen[1] == "squirrel" & 
-                          x$chosen[2] == "tomato"})
-
-names(include.subs) <- c("subid","include")
-
-# merges include column to data frame
-all.data <- merge(all.data,include.subs,sort = FALSE)
-
-# keeps just the subs who responded accurately on example trials
-keep.data <- subset(all.data,include)
-
-## flag first test trials for subjects ##  
-keep.data$first.trial <- FALSE
-keep.data$first.trial[(keep.data$interval=="Zero" & keep.data$trial.num==4) |
-                        (keep.data$interval=="One" & keep.data$trial.num==7) |
-                        (keep.data$interval=="Three" & keep.data$trial.num==9) |
-                        (keep.data$interval=="Seven" & keep.data$trial.num==13)] <- TRUE
-
-# creates numeric vars for data analysis 
-keep.data$numPicN <- as.numeric(keep.data$numPic)
-
-# create block variable
-keep.data$block <- ifelse(keep.data$itemNum <= 7, "familiarization", "test")
+df_final %<>%
+  mutate(correct = ifelse(trial_category == "exposure" & gaze_trial == "Gaze", gaze_target == chosen,
+                          ifelse(trial_category == "test", chosen == kept,
+                                 "NA")))
 
 ##### Anonymize workerids before moving to version control #######
 
 # grab worker ids and create anonymous id number
-anonymized_df <- keep.data %>% 
-  select(subid) %>% 
+anonymized_df <- df_final %>% 
+  select(workerid) %>% 
   distinct() %>% 
   mutate(subids = 1:nrow(.))
 
 # now join with original data frame
-df_final_clean <- left_join(keep.data, anonymized_df, by = "subid") 
-df_final_clean <- select(df_final_clean, -subid) %>% 
+df_final_clean <- left_join(df_final, anonymized_df, by = "workerid") 
+df_final_clean <- select(df_final_clean, -workerid) %>% 
   rename(subid = subids)
 
 ##### SAVE OUTPUT  #####
 
-write.csv(df_final_clean, paste(write_path, "e4_soc_xsit_inspection.csv", sep=""),row.names=FALSE)
+write.csv(df_final_clean, paste(write_path, "e4_soc_xsit_inspection.csv", sep=""), row.names=FALSE)
